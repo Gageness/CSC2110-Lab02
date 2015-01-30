b31bf6e2edcf465efc3c7f0d6e396426887003f0
Gageness <gageagarner@yahoo.com>
2015-01-30 02:04:31 -0600

    Added constructor and destructor, not working atm

--- ReadFile.h
+++ ReadFile.h
@@ -6,15 +6,18 @@
 #include <fstream>
 using namespace std;
 
-struct ReadFile
+class ReadFile
 {
-   ifstream input_file;
-   bool _eof;
-   bool closed;
+	private:
+		ifstream input_file;
+		bool _eof;
+		bool closed;
+	public:
+		ReadFile(const char* file_name);
+		~ReadFile();
 };
 
-ReadFile* createReadFile(const char* file_name);
-void destroyReadFile(ReadFile* rf);
+
 String* readLine(ReadFile* rf);
 bool eof(ReadFile* rf);
 void close(ReadFile* rf);
